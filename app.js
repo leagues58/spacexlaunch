bce99ba2cc524cacfe70881ac5111ccb54de5839
ericreeves <leagues58@gmail.com>
2018-03-03 19:09:06 -0500

    cleaned up code, added explanitory comments

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+/*****************************************************************************
+index.js
+creates an express application object (app), deals with some settings and 
+middleware assocatied with the app, then exports the app object.
+******************************************************************************/
 
+
+// modules we need
+var express       = require('express');
+var path          = require('path');
+var favicon       = require('serve-favicon');
+var logger        = require('morgan');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+
+// route modules we need
+var index = require('./routes/index.js');
+var users = require('./routes/users.js');
+
+// create the app
 var app = express();
 
 // disable telling how our app is served
 app.disable('x-powered-by');
 
-// view engine setup
+// view engine setup. first set up where the templates are stored, then set the template type
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// add some middleware
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// get express to serve up static contect from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// set up routes using previously imported modules
 app.use('/', index);
 app.use('/users', users);
 
@@ -46,4 +63,34 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+/*
+// connect to database
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+var mongoose = require('mongoose');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'spacexlaunch';
+
+//Import the mongoose module
+
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+*/
+
+// export the app object for use elsewhere (like www.js)
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,19 @@
+
+/*****************************************************************************
+index.js
+loads the express module, uses it to get a router object.  Then specify a route
+on that object and export it.  This export is used in app.js.
+The route has a callback which is invoked whenever a GET request happens.
+******************************************************************************/
+
+
 var express = require('express');
 var router = express.Router();
 
 
-
-
-var data = {
-  lauches: [
-    'launch1',
-    'launch2',
-    'launch3',
-    'launch4',
-    'launch5',
-    'launch6',
-    'launch7'
-  ]
-
-
-
-
-
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'spacexlaunch.org', data});
+  res.render('index', { title: 'spacexlaunch.org'});
 });
 
 module.exports = router;
