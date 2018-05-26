a14425dd7ebee62978fcab902629ed67ac923935
ericreeves <leagues58@gmail.com>
2018-05-26 12:05:06 -0400

    just testing git stuff

--- routes/index.js
+++ routes/index.js
@@ -15,46 +15,64 @@ var request       = require('request');
 let date      = new Date();
 let fileName  = "launchdata/" + date.getFullYear() + date.getMonth() + date.getDay() + date.getHours() + '.txt'
 
+var objLaunches;
+
+/* API endpoints
+all launches, past and future
+http://api.spacexdata.com/v2/launches/all
+
+future launches
+http://api.spacexdata.com/v2/launches/upcoming
+
+past launches
+http://api.spacexdata.com/v2/launches
+
+*/
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.stat(fileName, function(err, stat) {
-    if(err == null) {
-        console.log('file exists');
-    } else if(err.code == 'ENOENT') {
-        // file does not exist, so get the data a write to file
-        request('http://api.spacexdata.com/v2/launches/upcoming', { json: true }, (err, response, body) => {
 
-        fs.writeFile( fileName, JSON.stringify(body), function(err) {
+   // find out if this hour's data already exists
+   fs.stat(fileName, function(err, stat) {
+
+   // if so, load the file contents
+   if(err == null) {
+
+      fs.readFile(fileName, 'utf8', function (err, data) {
+         if (err) throw err;
+         console.log('reading file');
+         objLaunches = JSON.parse(data);
+         console.log('reading file, Emily');
+       });      
+
+   // if there is no file, hit the API and write contents to file
+   } else if(err.code == 'ENOENT') {
+      console.log('hitting endpoint');
+      // file does not exist, so get the data a write to file
+      request('http://api.spacexdata.com/v2/launches/upcoming', { json: true }, (err, response, body) => {
+         console.log('writing file with new data');
+         fs.writeFile( fileName, JSON.stringify(body), function(err) {
+
             if(err) {
-                return console.log(err);
+               console.log('error writing new data to file');
+               console.log(err);
             }
-        
+            objLaunches = JSON.parse(body);
             console.log("The file was saved!");
-        }); 
+         }); 
+
       });
 
-    } else {
-        console.log('Some other error: ', err.code);
-    }
-});
-  
-res.render('index', { title: '', datastuff: 'launchData'});
+   } 
+
+   res.render('index', { title: '', launches: objLaunches});
+
+   });
 });
 
 
 
 // export the route object
 module.exports = router;
-
-
-
-
-
-
-
-
-router.get('/', function(req, res, next) {
-    
-  });
