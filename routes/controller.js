4c3d016d15fdd4ef1528d60aa47fa9d68e3329aa
Eric Reeves <leagues58@gmail.com>
2018-10-13 21:48:54 -0400

    Revert "Added .gitignore, slight code cleanup. First time back in project for a while"
    This reverts commit 3396c2532486f51c70a37acae0b9a39ec0a00508.

--- routes/controller.js
+++ routes/controller.js
@@ -1,6 +1,8 @@
 var express     = require('express');
 var router      = express.Router();
 var launchModel = require('../models/launch_schema.js');
+//var bodyParser  = require('body-parser');
+
 
 var data = {};
 
