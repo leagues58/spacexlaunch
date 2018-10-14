cbddaf68ea998c8a8e8458aeb8e38677f3d677be
Eric Reeves <leagues58@gmail.com>
2018-10-14 13:22:13 -0400

    Another upgrade to index.js: used template literals, broke up more code into another function, slightly better variable naming, removed some unnecessary globals

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 
 /*****************************************************************************
 index.js
-loads the express module, uses it to get a router object.  Then specify a route
-on that object and export it.  This export is used in app.js.
-The route has a callback which is invoked whenever a GET request happens.
+Page retreives the launch data json from either a file on disk 
+(if the file) is less than 1 hour old, or hits the API endpoint to obtain a 
+fresh copy of the data, which it then writes to disk for next time. In either
+flow, the launch data is then passed to the index view.
 ******************************************************************************/
 /* API endpoints
 all launches, past and future
@@ -17,29 +18,29 @@ http://api.spacexdata.com/v2/launches
 
 */
 
-
-let express       = require('express');
-let router        = express.Router();
-let fileSystem    = require('fs');
-let request       = require('request');
-
-let launchDataLastSavedDate      = new Date();
-let launchDataFile  = "launchdata/" + launchDataLastSavedDate.getFullYear() + launchDataLastSavedDate.getMonth() + launchDataLastSavedDate.getDate() + launchDataLastSavedDate.getHours() + '.txt'
-
-let launchData;
+let express = require('express');
+let router = express.Router();
+let fileSystem = require('fs');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
+  let launchDataFilePath = getLaunchDataFilePath();
 
-  if (fileSystem.existsSync(launchDataFile)) {
-    readLaunchDataFromFile(launchDataFile).then(launchData => renderPage(res, launchData));
+  if (fileSystem.existsSync(launchDataFilePath)) {
+    readLaunchDataFromFile(launchDataFilePath).then(launchData => renderPage(res, launchData));
   } else {
-    getLaunchDataFromAPI(launchDataFile).then(launchData => renderPage(res, launchData));
+    getLaunchDataFromAPI(launchDataFilePath).then(launchData => renderPage(res, launchData));
   } 
   
 });
 
+
+function getLaunchDataFilePath() {
+  let currentDateTime = new Date();
+  return `launchdata/${currentDateTime.getFullYear()}${currentDateTime.getMonth()}${currentDateTime.getDate()}${currentDateTime.getHours()}.txt`;
+}
 
 function readLaunchDataFromFile(dataFile) {
   return new Promise((resolve, reject) => {
@@ -52,17 +53,22 @@ function readLaunchDataFromFile(dataFile) {
 
 function getLaunchDataFromAPI(dataFile) {
   return new Promise((resolve, reject) => {
+    let request = require('request');
     request('http://api.spacexdata.com/v2/launches/upcoming', { json: true }, (err, response, body) => {
       if (err) reject(err);
       else {
         resolve(body);
-        fileSystem.writeFile( dataFile, JSON.stringify(body), (err) => {}); 
+        fileSystem.writeFile( dataFile, JSON.stringify(body)); 
       }
     });
   });
 }
 
 function renderPage(res, launchData) {
+  launchData.forEach(element => {
+    //element.launch_date_utc = element.launch_date_utc;
+  });
+
   res.render('index', { title: '', launches: launchData});
 }
 
